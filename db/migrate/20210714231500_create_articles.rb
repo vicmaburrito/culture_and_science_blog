633ac2c7f7f6f256aed71ba39f689e200f26143f
vicmaburrito <vmaldaraca@gmail.com>
2021-07-14 18:15:48 -0500

    Add articles models

--- db/migrate/20210714231500_create_articles.rb
+++ db/migrate/20210714231500_create_articles.rb
@@ -1,14 +1,13 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
+      t.integer :author_id
       t.string :title
       t.text :text
       t.string :image
-      t.integer :author_id
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :articles, :user_id
   end
 end
