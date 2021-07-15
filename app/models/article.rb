43310f6aa44e5afd427488f7e8d86aa99c5f8145
vicmaburrito <vmaldaraca@gmail.com>
2021-07-15 15:54:57 -0500

    Fix models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  validates :title, presence: true
+  validates :text, presence: true
+  has_one_attached :image
+
 end
