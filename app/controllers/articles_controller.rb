dd1bfb54c1ef452fb13dbaa8009b93abcde5012a
vicmaburrito <vmaldaraca@gmail.com>
2021-07-14 23:49:01 -0500

    Fix linters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,47 +1,47 @@
 class ArticlesController < ApplicationController
   before_action :set_current_user
 
-    def index
-        @articles = Article.all
-      end
-    
-      def new
-        @article = Article.new
-      end
-    
-      def create
-        @article = Article.new(user_params)
-    
-        if @article.save
-          redirect_to root_path
-        else
-          render :new
-        end
-      end
-    
-      def edit
-        @article = Article.find(params[:id])
-      end
-    
-      def update
-        @article = Article.find(params[:id])
-    
-        if @article.update(user_params)
-          redirect_to root_path
-        else
-          render :edit
-        end
-      end
+  def index
+    @articles = Article.all
+  end
 
-    def destroy
-      @article = Article.find(params[:id])
-       @article.destroy
-       redirect_to root_path
-    end
+  def new
+    @article = Article.new
+  end
 
-      private
-    
-      def user_params
-        params.require(:article).permit(:title, :text, :image)
-      end
+  def create
+    @article = Article.new(user_params)
+
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:article).permit(:title, :text, :image)
+  end
+end
