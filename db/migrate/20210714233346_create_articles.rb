5af029310117c182fb2198a6dabbb09932a36503
vicmaburrito <vmaldaraca@gmail.com>
2021-07-15 14:46:55 -0500

    Fix Articles database

--- db/migrate/20210714233346_create_articles.rb
+++ db/migrate/20210714233346_create_articles.rb
@@ -3,8 +3,8 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.string :title
       t.text :text
-      t.string :image
-      t.references :user, null: false, foreign_key: true
+      t.belongs_to :user
+      t.belongs_to :category
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,11 @@ ActiveRecord::Schema.define(version: 2021_07_15_191329) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.string "image"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
+    t.bigint "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -38,5 +39,4 @@ ActiveRecord::Schema.define(version: 2021_07_15_191329) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "articles", "users"
 end
