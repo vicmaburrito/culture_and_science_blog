01f440e7eb27b0ab3d985027ef46181a40f4051a
vicmaburrito <vmaldaraca@gmail.com>
2021-07-14 21:55:21 -0500

    Edit views & routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create edit update]
   resources :articles, only: [:new, :create, :index]
   resources :articles
-  root 'users#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
