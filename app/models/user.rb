cca6691aaf656c67e0ed692e9f34d65048eee718
vicmaburrito <vmaldaraca@gmail.com>
2021-07-17 14:37:21 -0500

    Add methods into models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 15 },
                        format: { with: /\A[\w-]+\z/, message: 'your name requirements' }
   has_many :articles
+  has_many :votes
+
+  def votes?
+    article.votes.where(user_id: id).any?
+  end
 end
