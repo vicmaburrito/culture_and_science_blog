83c78c5668745d15d4532a7835c48e78b2904b88
vicmaburrito <vmaldaraca@gmail.com>
2021-07-19 15:35:34 -0500

    Render grid and start working layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+    @top = Article.top_article
     @categories = Category.order(:priority).limit(6).includes(:articles)
   end
 
