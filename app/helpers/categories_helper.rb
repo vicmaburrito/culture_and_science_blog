fc7134f077531708d6bc0736122b3a4ed69bf234
vicmaburrito <vmaldaraca@gmail.com>
2021-07-20 00:53:39 -0500

    Create query to title & add into articles index

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,20 @@
 module CategoriesHelper
+    def history_path(history)
+        history = Category.find(1)
+    end
+    def engineering_path(engineering)
+        engineering = Category.find(3)
+    end
+
+    def art_path(art)
+       art = Category.find(4)
+    end
+
+    def philosophy_path(philosophy)    
+       philosophy = Category.find(2)
+    end
+
+    def psychology_path(psychology)
+        psychology = Category.find(6)
+    end
 end
