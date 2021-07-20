7565f1e0bf89cc8886b7f21d431897469edaa16f
vicmaburrito <vmaldaraca@gmail.com>
2021-07-20 01:27:19 -0500

    Fix rubocop

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,6 +2,7 @@ module ArticlesHelper
   def number_of_votes(article)
     pluralize(article.votes.size, 'vote')
   end
+
   def top_image(article)
     url_for(article.try(:image).blob)
   end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,20 +1,21 @@
 module CategoriesHelper
-    def history_path(history)
-        history = Category.find(1)
-    end
-    def engineering_path(engineering)
-        engineering = Category.find(3)
-    end
+  def history_path(_history)
+    history = Category.find(1)
+  end
 
-    def art_path(art)
-       art = Category.find(4)
-    end
+  def engineering_path(_engineering)
+    engineering = Category.find(3)
+  end
 
-    def philosophy_path(philosophy)    
-       philosophy = Category.find(2)
-    end
+  def art_path(_art)
+    art = Category.find(4)
+  end
 
-    def psychology_path(psychology)
-        psychology = Category.find(6)
-    end
+  def philosophy_path(_philosophy)
+    philosophy = Category.find(2)
+  end
+
+  def psychology_path(_psychology)
+    psychology = Category.find(6)
+  end
 end
