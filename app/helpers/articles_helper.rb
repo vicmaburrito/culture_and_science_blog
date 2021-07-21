926bd138b42c61820dc930d7e50f202e1ec9e23f
vicmaburrito <vmaldaraca@gmail.com>
2021-07-20 23:30:46 -0500

    Add author username

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,9 @@ module ArticlesHelper
   def number_of_votes(article)
     pluralize(article.votes.size, 'vote')
   end
-
+  def author_name(article)
+    article.user.username
+  end
   def top_image(article)
     url_for(article.try(:image).blob)
   end
