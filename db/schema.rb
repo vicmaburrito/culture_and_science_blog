e6acbea4268f623056db2832c55833615bdfde22
vicmaburrito <vmaldaraca@gmail.com>
2021-07-21 11:57:32 -0500

    Create new model votes

--- db/schema.rb
+++ db/schema.rb
@@ -67,15 +67,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_205152) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.bigint "article_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["article_id"], name: "index_votes_on_article_id"
-    t.index ["user_id"], name: "index_votes_on_user_id"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
