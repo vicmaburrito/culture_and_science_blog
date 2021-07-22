27d4db39b6cc882adc09cb4e81696554ef903e1c
vicmaburrito <vmaldaraca@gmail.com>
2021-07-22 01:08:56 -0500

    Create vote model

---
+++ db/migrate/20210722060439_create_votes.rb
@@ -0,0 +1,9 @@
+class CreateVotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :votes do |t|
+      t.belongs_to :article
+      t.belongs_to :user
+
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_165504) do
+ActiveRecord::Schema.define(version: 2021_07_22_060439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,8 +70,6 @@ ActiveRecord::Schema.define(version: 2021_07_21_165504) do
   create_table "votes", force: :cascade do |t|
     t.bigint "article_id"
     t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_votes_on_article_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
